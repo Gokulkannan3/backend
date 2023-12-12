ac72259d0064d87db6c849ca87a07bab59707cd2
Gokul Kannan P <91548753+Gokulkannan3@users.noreply.github.com>
2023-12-12 21:06:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
-    password: 'Gokul@003',
+    password: '',
     database: 'screening',
 });
 
